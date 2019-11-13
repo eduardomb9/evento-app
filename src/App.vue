1c181f952b1d5a30451cfcf8c9e4ec862cf116ef
eduardomb9 <maranhaoeduardo9@gmail.com>
2019-11-13 15:19:46 -0300

    ajuste na navegacao

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,18 @@
 <template>
     <v-app>
-      <v-tabs v-if="!login" dark>
-        <v-tab @click="$router.push('/home')">
-          Eventos
-        </v-tab>
-        <v-tab @click="$router.push('/participantes')">
-          Participantes
-        </v-tab>
-        <v-tab right @click="resetToken()" class="float-right"><v-icon color="dark">mdi-power</v-icon></v-tab>
-      </v-tabs>
-      
+      <HeaderNav v-if="$router.currentRoute.name != 'login'" />
+
       <router-view />
     </v-app>
 </template>
 
 <script>
+import HeaderNav from "./components/HeaderNav";
+
 export default {
-  data: function () {
-    return {
-      login: false,
-    }
-  },
-  methods: {
-    resetToken: function () {
-      sessionStorage.removeItem('token')
-      this.login = true
-      this.$router.push('/')
-    },
+  components: {
+    HeaderNav
   }
+  
 }
 </script>
---
+++ src/components/HeaderNav.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-tabs dark>
+        <v-tab @click="$router.push('/home')">
+          Eventos
+        </v-tab>
+        <v-tab @click="$router.push('/participantes')">
+          Participantes
+        </v-tab>
+        <v-tab right @click="resetToken()" class="float-right"><v-icon color="dark">mdi-power</v-icon></v-tab>
+      </v-tabs>
+</template>
+
+<script>
+export default {
+    methods: {
+        resetToken: function () {
+        sessionStorage.removeItem('token')
+        this.$router.push('/')
+        },
+    }
+}
+</script>
