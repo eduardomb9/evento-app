408afb86e2141e6999225d6a0f4cf6c0af151689
eduardomb9 <maranhaoeduardo9@gmail.com>
2020-01-02 16:34:57 -0300

    removendo codigo teste

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,6 @@ import Router from 'vue-router'
 import Home from '../view/Home'
 import Participantes from '../view/Participantes'
 import Login from '../view/Login'
-import Promises from '../view/Promises'
 
 Vue.use(Router)
 
@@ -18,11 +17,6 @@ const routes = [
         name: 'participantes',
         path: '/participantes',
         component: Participantes
-    },
-    {
-        name: 'promiseTest',
-        path: '/promises',
-        component: Promises
     },
     {
         name: 'home',
